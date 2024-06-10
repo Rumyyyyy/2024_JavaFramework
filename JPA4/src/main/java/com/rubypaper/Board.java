3f71ba55ddbe8b3765303fb7107768fc6ce05a03
Rumyyyyy <dkfmadl1219@naver.com>
2024-06-10 11:47:29 +0900

    git commit

--- JPA4/src/main/java/com/rubypaper/Board.java
+++ JPA4/src/main/java/com/rubypaper/Board.java
@@ -2,20 +2,34 @@ package com.rubypaper;
 
 import java.util.Date;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.persistence.Transient;
 import lombok.Data;
 
 @Data  //setter/getter
 @Entity  //db의 엔티티(객체)
-public class Board {
+// @Table(name = "BoardT")
+@SequenceGenerator(name = "board_seq2" , sequenceName = "Board_Sequence" , 
+				initialValue = 1, allocationSize=1)
+public class Board { // JPA 컨테이너
 	@Id              //pk
-	@GeneratedValue  //1씩 자동증가
+	@GeneratedValue // (strategy = GenerationType.SEQUENCE, // INDENTITY mysql
+					 // generator = "board_seq2")  //1씩 자동증가
 	 private int seq;
+	@Column (unique = true)
 	 private String title;
 	 private String writer;
 	 private String content;
+	 @Temporal(TemporalType.DATE)
 	 private Date createDate;
 	 private int cnt;
+	 @Transient
+	 private String hint;
 }
--- JPA4/src/main/java/com/rubypaper/BoardServiceJPA.java
+++ JPA4/src/main/java/com/rubypaper/BoardServiceJPA.java
@@ -1,6 +1,6 @@
 package com.rubypaper;
 
-import java.util.List;
+import java.util.Date;
 
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -18,32 +18,45 @@ public class BoardServiceJPA implements ApplicationRunner{
 		EntityManagerFactory emf =
 				Persistence.createEntityManagerFactory("JPA4");
 		EntityManager em = emf.createEntityManager();	
-		// em.find 검색
-		Board findBoard = em.find(Board.class, 52);
-		System.out.println("검색 결과 :"+findBoard.toString());
-		// 수정하기
-		EntityTransaction tx = em.getTransaction();
+		EntityTransaction tx =em.getTransaction();
 		tx.begin();
-		findBoard.setTitle("---titlechange52---");
-		findBoard.setContent("change content54");
-		findBoard.setWriter("김유한");
-		tx.commit();
-		System.out.println("수정결과값 : "+findBoard.toString());
+			Board board = new Board();
+			// board.setSeq(3);
+			board.setTitle("3-1반 스프링");
+			board.setWriter("kim maldong");
+			board.setCreateDate(new Date());
+			board.setContent("title1");
+			board.setCnt(0);
+			System.out.println("persist 명령 전 상태 :" +em.contains(board));
+			// persist에 의해 영속상태로 이동JPA가 관리하고 있음
+			em.persist(board);
+			tx.commit();
+			Board findBoard = em.find(Board.class, 1);
+			// friendBoard = em.find(Board.class, 2);
+			System.out.println("--------------------");
+			System.out.println("찾은 값은 : "+findBoard.toString());
+			System.out.println("find 명령 후 상태 :" +em.contains(findBoard));
+			em.detach(findBoard);
+			System.out.println("detach 명령 후 상태 :" +em.contains(findBoard));
+			
+			// friendBoard.setTitle("[title~~~]");
+			Board mergeBoard = em.merge(findBoard);
+			System.out.println("merge 후 findBoard는 영속 상태인가 : "+em.contains(findBoard));
+			System.out.println("merge 후 mergeBoard는 영속 상태인가 : "+em.contains(mergeBoard));
+			
+			em.close();
+			emf.close();
+		// em.find 검색
+		// Board board = new Board();
+		// Board findBoard = em.find(Board.class, 52);
+		// System.out.println("검색 결과 값 :"+findBoard.toString());
 		// 삭제하기
 		// em.remove(findBoard);
 		
-		// 글 목록 다 출력
-		String jpq1 = "select b from Board order by b.seq desc";
-		List<Board> Boardlist = em.createQuery(jpq1).getResultList();
-		for(Board board1:Boardlist) {
-			System.out.println("------>"+board1.toString());
-		}
-		tx.commit();
-		em.close();
-		emf.close();
 		// insert
 //		EntityTransaction tx =em.getTransaction();
 //		tx.begin();
+		// 정보삽입
 //		Board board = new Board();	
 //	    board.setTitle("jpatitle3");
 //	    board.setWriter("kim uhan3");
